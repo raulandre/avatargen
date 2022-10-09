ed776d97eda4e05654c77cd87c7bdb4d275e3eb8
Raul André <raulandre.dev@gmail.com>
2022-10-08 22:16:13 -0300

    Project setup

---
+++ lib/avatargen.ex
@@ -0,0 +1,18 @@
+defmodule Avatargen do
+  @moduledoc """
+  Documentation for `Avatargen`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Avatargen.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/avatargen_test.exs
@@ -0,0 +1,8 @@
+defmodule AvatargenTest do
+  use ExUnit.Case
+  doctest Avatargen
+
+  test "greets the world" do
+    assert Avatargen.hello() == :world
+  end
+end
